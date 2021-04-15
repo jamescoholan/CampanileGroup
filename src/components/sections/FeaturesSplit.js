2a9c14c8780c4bef0dbc6bd8168a7cf3a0f3e65e
James Coholan <jamescoholan@gmail.com>
2021-04-15 11:28:37 -0700

    commit on 'main'

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionSplitProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
-import Image from '../elements/Image';
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
 
 const propTypes = {
-  ...SectionSplitProps.types
-}
+  ...SectionSplitProps.types,
+};
 
 const defaultProps = {
-  ...SectionSplitProps.defaults
-}
+  ...SectionSplitProps.defaults,
+};
 
 const FeaturesSplit = ({
   className,
@@ -26,128 +26,171 @@ const FeaturesSplit = ({
   imageFill,
   ...props
 }) => {
-
   const outerClasses = classNames(
-    'features-split section',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'features-split-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
+    "features-split-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
   );
 
   const splitClasses = classNames(
-    'split-wrap',
-    invertMobile && 'invert-mobile',
-    invertDesktop && 'invert-desktop',
-    alignTop && 'align-top'
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
   );
 
   const sectionHeader = {
-    title: 'Workflow that just works',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+    title: "Workflow that just works",
+    paragraph: "Best in Class Service.",
   };
 
   return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
+    <section {...props} className={outerClasses}>
       <div className="container">
         <div className={innerClasses}>
           <SectionHeader data={sectionHeader} className="center-content" />
           <div className={splitClasses}>
-
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Lightning fast workflow
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Data-driven insights
-                  </h3>
+                </div>
+                <h3 className="mt-0 mb-12">One Stop Digital Pipeline</h3>
+
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                  Webpage Tailoring & Search Engine Optimization
+                </p>
+                <p className="m-0">Workflow Optimization</p>
+                <p className="m-0">Mobile Application Development</p>
+                <p className="m-0">Social Media Consulting</p>
+                <p className="m-0">Content Development.</p>
+                <p className="m-0">3D and Motion Graphics.</p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/features-split-image-01.png')}
+                  src={require("./../../assets/images/Screen Shot 2021-04-15 at 10.39.34 AM.png")}
                   alt="Features split 01"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Lightning fast workflow
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Data-driven insights
-                  </h3>
-                <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                </div>
+                <h3 className="mt-0 mb-12">Proprietary Policy Research</h3>
+                <p className="m-0">Data Science Reporting & Visualizations</p>
+                <p className="m-0">Mission-Driven Copywriting</p>
+                <p className="m-0">Community Needs Assessment</p>
+                <p className="m-0">Impact Measurement</p>
+                <p className="m-0">Cross-Sector Collaborations</p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/features-split-image-02.png')}
+                  src={require("./../../assets/images/Screen Shot 2021-04-15 at 10.39.42 AM.png")}
                   alt="Features split 02"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Lightning fast workflow
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Data-driven insights
-                  </h3>
-                <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                </div>
+                <h3 className="mt-0 mb-12">Values Based Initiatives</h3>
+                <p className="m-0">Partnership Evaluation & Outreach</p>
+                <p className="m-0">Media Partnerships</p>
+                <p className="m-0">Graphic Design</p>
+                <p className="m-0">Logo Design & Visuals ​</p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/features-split-image-03.png')}
+                  src={require("./../../assets/images/Screen Shot 2021-04-15 at 10.39.50 AM.png")}
                   alt="Features split 03"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
-
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                </div>
+                <h3 className="mt-0 mb-12">Fundraising</h3>
+                <p className="m-0">Donation Page Optimization</p>
+                <p className="m-0">Grassroots Mobilization</p>
+                <p className="m-0">Grantmaking Support</p>
+                <p className="m-0">Peer-to-Peer Fundraising​</p>
+                <p className="m-0">Targeted Email Campaigns ​</p>
+                <p className="m-0">What Could My Money Do? Campaigns​</p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/Screen Shot 2021-04-15 at 10.39.57 AM.png")}
+                  alt="Features split 03"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
 FeaturesSplit.propTypes = propTypes;
 FeaturesSplit.defaultProps = defaultProps;
